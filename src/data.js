61bb115c9c03b502f55eaa32855b1820ff9ae96c
Chaitanya Naik <chaitanyanaik80@gmail.com>
2023-12-30 21:53:30 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,6 @@
+let moviesList = ['SALAAR', 'DUNKI', 'ANIMAL', 'AQUAMAN', 'Samबहादुर'];
+let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM"];
+let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
+exports.moviesList = moviesList;
+exports.slots = slots;
+exports.seats = seats;
